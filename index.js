2372d21a1b9694278126c1731a7004e41dd1914c
ShaneAlvares <frankshanealvares5@gmail.com>
2023-03-10 20:04:12 +0530

    first commiit

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ app.post('/handleJson', (req,res) => {
     res.send(req.body.name);
 });
 //Deploying the listener
-const port = process.env.port || 8081;
+const port = process.env.PORT || 8081;
 app.listen(port, () => console.log(`Express server listening on port ${port}`));
